fdd352ed1902df3cac638fd4172125769065ec52
David Diniz <diniz.david@gmail.com>
2020-12-19 16:29:13 -0300

    Doctrine migration setup

---
+++ migrations.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Configuração para migration no Doctrine
+ * @see https://www.doctrine-project.org/projects/doctrine-migrations/en/3.0/reference/configuration.html#migrations-configuration
+ */
+return [
+    'table_storage' => [
+        'table_name' => 'doctrine_migration_versions',
+        'version_column_name' => 'version',
+        'version_column_length' => 1024,
+        'executed_at_column_name' => 'executed_at',
+        'execution_time_column_name' => 'execution_time',
+    ],
+    'migrations_paths' => [
+        'CatApi\Migrations' => __DIR__ . '/db/migrations',
+    ],
+    'all_or_nothing' => true,
+    'check_database_platform' => true
+];
